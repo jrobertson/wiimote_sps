814d5bb9ca0c8ec792a8b5ab6f6cc97033fb9050
Your Name <you@example.com>
2015-06-16 13:18:36 +0100

    feature: Now subscribes to the SPS topic wiimote/output/#

--- lib/wiimote_sps.rb
+++ lib/wiimote_sps.rb
@@ -3,6 +3,7 @@
 # file: wiimote_sps.rb
 
 require 'sps-pub'
+require 'sps-sub'
 require 'simple_wiimote'
 
 
@@ -14,14 +15,47 @@ class WiimoteSps < SimpleWiimote
     super()
     @device_id = device_id
     @pub = SPSPub.new address: sps_address
+    @sub = SPSSub.new address: sps_address, callback: self
+
+
     @pub.notice @device_id + '/info: wiimote initialized'
 
   end
+  
+  def activate
+
+    Thread.new do      
+      @sub.subscribe topic: 'wiimote/output/#'
+    end
+        
+    super()
+
+  end
+  
+  def ontopic(topic, msg)
+
+    case topic
+    when /rumble$/
+     
+      case msg
+      when /^buzz/
+        self.rumble = true
+        sleep 0.3
+        self.rumble = false
+      end      
+
+    when /led$/
+      case msg
+      when /(\d+)\s+(on|off)/
+        self.led = ($2) == 'on' ? 1 : 0
+      end
+    end   
+  end     
 
   def notify(m)
-    @pub.notice @device_id + '/input: ' + m
-  end
-
+    @pub.notice @device_id + '/input: ' + m    
+  end  
+  
   def on_btn_2_up(wm)      notify 'button 2 pressed'     end
   def on_btn_1_up(wm)      notify 'button 1 pressed'     end
   def on_btn_b_press(wm)   notify 'button b pressed'     end
@@ -29,14 +63,16 @@ class WiimoteSps < SimpleWiimote
   def on_btn_b_down(wm)   end
   def on_btn_a_press(wm)      notify 'button a pressed'     end
   def on_btn_minus_press(wm)  notify 'button minus pressed' end
-  def on_btn_void1_press(wm)  notify 'button void1 pressed' end
-  def on_btn_void2_press(wm)  notify 'button void2 pressed' end
   def on_btn_home_press(wm)   notify 'button home pressed'  end
   def on_btn_left_press(wm)   notify 'button left pressed'  end
   def on_btn_right_press(wm)  notify 'button right pressed' end
   def on_btn_down_press(wm)   notify 'button down pressed'  end
   def on_btn_up_press(wm)     notify 'button up pressed'    end
   def on_btn_plus_press(wm)   notify 'button plus pressed'  end
-
+    
+  def on_deactivated()
+    EventMachine.stop
+  end
+  
   
 end
--- wiimote_sps.gemspec
+++ wiimote_sps.gemspec
@@ -1,11 +1,12 @@
 Gem::Specification.new do |s|
   s.name = 'wiimote_sps'
-  s.version = '0.1.2'
+  s.version = '0.2.0'
   s.summary = 'Uses a Wiimote to publish key presses to a SimplePubSub broker.'
   s.authors = ['James Robertson']
   s.files = Dir['lib/wiimote_sps.rb']
   s.add_runtime_dependency('sps-pub', '~> 0.4', '>=0.4.0')
-  s.add_runtime_dependency('simple_wiimote', '~> 0.3', '>=0.3.2')
+  s.add_runtime_dependency('sps-sub', '~> 0.2', '>=0.2.0')
+  s.add_runtime_dependency('simple_wiimote', '~> 0.3', '>=0.3.3')
   s.signing_key = '../privatekeys/wiimote_sps.pem'
   s.cert_chain  = ['gem-public_cert.pem']
   s.license = 'MIT'
